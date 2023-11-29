3fc5ef99990969e221910504ded479de5089e8f3
Shashank Singh <singhshashank875@gmail.com>
2023-11-29 21:52:27 +0530

    Localhost working properly after fixing http

--- functions/index.js
+++ functions/index.js
@@ -5,8 +5,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 // eslint-disable-next-line max-len
 const stripe = require("stripe")("sk_test_51ODPWcSBh8ydk88lQEMIAdWNUYB8eJ5C4xsWYCO8BYQRVwUnrMhUnU1geXk9aUcIr8nbs7Z1TfjaBEZRFyjih78d00PbLe2dxi");
+
 const corsOptions = {
-  origin: ["https://fir-f4f2f.web.app", "https://localhost:3000", "https://localhost:5000"],
+  origin: ["http://fir-f4f2f.web.app", "http://localhost:3000", "http://localhost:5000"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
@@ -26,19 +27,25 @@ app.get("/", (req, res) => {
 });
 
 app.post("/payments/create", async (req, res) => {
-  const total = req.query.total;
-  console.log("Payment Req Received", total);
+  try {
+    const total = req.query.total;
+    console.log("Payment Req Received", total);
 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: total*100,
-    currency: "INR",
-  });
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total * 100,
+      currency: "INR",
+    });
 
-  res.status(201).send({clientSecret: paymentIntent.client_secret});
+    res.status(201).send({clientSecret: paymentIntent.client_secret});
+  } catch (error) {
+    console.error("Error creating payment intent:", error);
+    res.status(500).send({error: "Internal Server Error"});
+  }
 });
+
 
 // Export will give an app link to get line 19
 exports.api = functions.https.onRequest(app);
 
 // example endpoint
-// https://127.0.0.1:5001/fir-f4f2f/us-central1/api
+// http://127.0.0.1:5001/fir-f4f2f/us-central1/api
