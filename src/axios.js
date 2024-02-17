63492e39a02121f468725944edbd3dd11916e3dd
Shashank Singh <singhshashank875@gmail.com>
2024-02-17 22:06:29 +0530

    100% completed. Lets host now

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://127.0.0.1:5001/aclone-2c628/us-central1/api",
+    baseURL: "https://us-central1-aclone-2c628.cloudfunctions.net/api",
+    // "http://127.0.0.1:5001/aclone-2c628/us-central1/api",
     timeout: 50000, // Adjust the timeout value as needed
 });
 
